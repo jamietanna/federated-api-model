a506836d6d04259e984e006356c71dedbe2ab084
Jamie Tanna <jamie@jamietanna.co.uk>
2022-10-03 09:16:09 +0100

    sq

---
+++ examples/go/internal/server/api/generate.go
@@ -0,0 +1,3 @@
+package api
+
+//go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen --config oapi-codegen.yaml ../../../../../schemas/v1alpha/openapi.yml
---
+++ examples/go/internal/server/router.go
@@ -0,0 +1,7 @@
+package server
+
+import "net/http"
+
+func NewRouter(deps any) http.Handler {
+	return nil
+}
